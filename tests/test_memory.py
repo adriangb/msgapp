db1e77fc88dbf6a78d00c239e10bae5730c956c0
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-08-10 05:36:36 -0500

    add example with anyio streams

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -1,5 +1,6 @@
 from typing import Any, AsyncIterable, List
 
+import anyio
 import pytest
 from pydantic import BaseModel
 
@@ -14,7 +15,7 @@ class MyModel(BaseModel):
 
 
 @pytest.mark.anyio
-async def test_consume_messages() -> None:
+async def test_consume_messages_generator() -> None:
     async def source() -> AsyncIterable[bytes]:
         yield b'{"foo": "bar", "baz": 1}'
 
@@ -30,6 +31,31 @@ async def test_consume_messages() -> None:
     )
 
     await app.run()
+
+    assert received == [MyModel(foo="bar", baz=1)]
+
+
+@pytest.mark.anyio
+async def test_consume_messages_queue() -> None:
+    send, rcv = anyio.create_memory_object_stream(0, item_type=bytes)
+
+    received: List[Any] = []
+
+    async def handler(message: MyModel) -> None:
+        received.append(message)
+
+    app = App(
+        handler,
+        producer=InMemoryQueue(rcv),
+        parser=PydanticParserFactory(),
+    )
+
+    async with rcv:
+        async with send:
+            async with anyio.create_task_group() as tg:
+                tg.start_soon(app.run)
+                async with send:
+                    await send.send(b'{"foo": "bar", "baz": 1}')
 
     assert received == [MyModel(foo="bar", baz=1)]
 

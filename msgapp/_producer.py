4efc6ca38a12da5e4171f122ae17ddaa6b173f60
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-09-13 00:29:36 -0500

    Remove extra import

--- msgapp/_producer.py
+++ msgapp/_producer.py
@@ -1,5 +1,5 @@
 import sys
-from typing import AsyncContextManager, AsyncIterable, Protocol, TypeVar
+from typing import AsyncContextManager, AsyncIterable, TypeVar
 if sys.version_info < (3, 8):
     from typing_extensions import Protocol
 else:
